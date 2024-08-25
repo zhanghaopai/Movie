18fd65c14478565ace5b31c87d92be3e44998ee9
pai_v98 <zhanghaopai@outlook.com>
2024-08-25 11:29:07 +0800

    修改

--- App.js
+++ App.js
@@ -2,13 +2,12 @@ import AppNavigation from './navigations/appNavigation';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { GlobalJS } from './style/global';
 import HomeScreen from "./screens/HomeScreen";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView >
-        <HomeScreen />
-      </SafeAreaView>
+      <HomeScreen/>
     </SafeAreaProvider>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,14 @@
 import { Bars3CenterLeftIcon } from "@heroicons/react/16/solid";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
     return (
-        <View>
-            <SafeAreaView  className="flex-1">
-                <StatusBar></StatusBar>
-                <Text className="text-blue-400">HomeScreen</Text>
-            </SafeAreaView>
-        </View>
+        <SafeAreaView>
+            {/* <StatusBar style="light"></StatusBar> */}
+            <Text>kkkkkkkk</Text>
+        </SafeAreaView>
     );
 }
